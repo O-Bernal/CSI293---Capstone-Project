ce44439fa17d7e1a4a2437cdead7a4111ccb3b61
O-Bernal <103008336+O-Bernal@users.noreply.github.com>
2023-05-19 01:58:50 -0700

    Update InputArea.cs
    The script for our Hit Zone game objects now have logic for hitting notes within a range of early - missed notes.

--- Melody Riders/Assets/Scenes/Scripts/Input Scripts/InputArea.cs
+++ Melody Riders/Assets/Scenes/Scripts/Input Scripts/InputArea.cs
@@ -5,33 +5,35 @@ using UnityEngine;
 public class InputArea : MonoBehaviour
 {
     public KeyCode hitKey;
-
     private NoteMovement currentNote;
     public NoteSpawner.Lane lane;
     public float perfectRange = 0.2f;
+    public float goodRange = 0.3f;
     public float earlyLateRange = 0.4f;
 
     private void OnTriggerEnter(Collider other)
     {
         NoteMovement note = other.GetComponent<NoteMovement>();
-        if (note.lane == lane)
+        if (note && note.lane == this.lane)
         {
             currentNote = note;
+            //Debug.Log($"Note entered {lane} trigger");
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Note"))
+        NoteMovement note = other.GetComponent<NoteMovement>();
+        if (note && note.lane == this.lane)
         {
             // Register a miss if the note wasn't hit
             if (currentNote)
             {
-                Debug.Log("Missed!");
+                Debug.Log($"{lane} Missed!");
                 Destroy(currentNote.gameObject);
+                currentNote = null;
             }
-
-            currentNote = null;
+            //Debug.Log($"Note exited {lane} trigger");
         }
     }
 
@@ -42,25 +44,49 @@ public class InputArea : MonoBehaviour
             // Determine how well-timed the hit was based on the note's position
             // Calculate the difference between the note's position and the hit point
             float hitDifference = Mathf.Abs(transform.position.z - currentNote.transform.position.z);
+            bool wasHit = false;
+            Debug.Log($"{lane} Hit difference: " + hitDifference);
 
             if (hitDifference <= perfectRange)
             {
-                Debug.Log("Perfect!");
+                Debug.Log($"{lane} Perfect!");
+                Destroy(currentNote.gameObject);
+                currentNote = null;
+            }
+            else if (hitDifference <= goodRange)
+            {
+                Debug.Log($"{lane} Good!");
+                Destroy(currentNote.gameObject);
+                currentNote = null;
             }
             else if (hitDifference <= earlyLateRange)
             {
+                wasHit = true;
                 if (currentNote.transform.position.z > transform.position.z)
                 {
-                    Debug.Log("Early!");
+                    Debug.Log($"{lane} Early!");
+                    Destroy(currentNote.gameObject);
+                    currentNote = null;
                 }
                 else
                 {
-                    Debug.Log("Late!");
+                    Debug.Log($"{lane} Late!");
+                    Destroy(currentNote.gameObject);
+                    currentNote = null;
                 }
             }
+            else
+            {
+                Debug.Log($"{lane} Missed!");
+                Destroy(currentNote.gameObject);
+            }
 
-            // Destroy the note
-            Destroy(currentNote.gameObject);
+            // Add to score only if the note was not hit early or late
+            if (!wasHit)
+            {
+                // Add to score here
+            }
         }
     }
 }
+
