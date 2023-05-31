414fc319e7847b3636c76304210033e8c4ccf666
O-Bernal <103008336+O-Bernal@users.noreply.github.com>
2023-05-31 00:12:04 -0700

    Database Files
    The files in the Migrations folder are created during PMC migrations and update database operations
    Created GameDbContext

---
+++ MelodyRider_Back-End_System/MelodyRider_Back-End_System/Data/GameDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MelodyRider_Back_End_System.Models;
+
+namespace MelodyRider_Back_End_System.Data
+{
+    public class GameDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Score> Scores { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Achievement> Achievements { get; set; }
+        public DbSet<UserAchievement> UserAchievements { get; set; }
+
+        public GameDbContext(DbContextOptions<GameDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserAchievement>()
+                .HasKey(ua => new { ua.UserId, ua.AchievementId });
+
+            // Other model configurations...
+        }
+    }
+}
