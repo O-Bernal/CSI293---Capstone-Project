e42f75495faae16b6c7e68cfdd1c1c3693f35eea
O-Bernal <103008336+O-Bernal@users.noreply.github.com>
2023-06-15 02:49:32 -0700

    Profile and Settings update
    -I combined the two views into one. The user settings page has the profile badge next to the settings form that allows users to change and update their information This page is fully functional.

---
+++ MelodyRider_Back-End_System/MelodyRider_Back-End_System/wwwroot/js/settings.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    // Prevent form submission if username or email fields are empty
+    $('#settings-form').on('submit', function (e) {
+        var username = $('#username').val();
+        var email = $('#email').val();
+        var currentPassword = $('#oldPassword').val();
+        var newPassword = $('#newPassword').val();
+        var confirmPassword = $('#confirmPassword').val();
+
+        if (!username || !email) {
+            alert('Username and email fields cannot be empty.');
+            e.preventDefault();
+        }
+
+        if ((newPassword || confirmPassword) && !currentPassword) {
+            alert('You must enter your current password to change your password.');
+            e.preventDefault();
+        }
+    });
+
+    // Add an 'edit' button to toggle the editability of the username and email fields
+    $('#edit-username').click(function () {
+        var usernameInput = $('#username');
+        usernameInput.prop('readonly', !usernameInput.prop('readonly'));
+    });
+    $('#edit-email').click(function () {
+        var emailInput = $('#email');
+        emailInput.prop('readonly', !emailInput.prop('readonly'));
+    });
+});
