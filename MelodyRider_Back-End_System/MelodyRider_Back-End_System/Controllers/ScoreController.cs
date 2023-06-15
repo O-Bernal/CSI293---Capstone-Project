74153d21a4fc28b8b3954dfdf43656c298bab84f
O-Bernal <103008336+O-Bernal@users.noreply.github.com>
2023-06-15 02:42:25 -0700

    Update ScoreController.cs
    using statements, but currently my score controller is not implemented.

--- MelodyRider_Back-End_System/MelodyRider_Back-End_System/Controllers/ScoreController.cs
+++ MelodyRider_Back-End_System/MelodyRider_Back-End_System/Controllers/ScoreController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MelodyRider_Back_End_System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MelodyRider_Back_End_System.Controllers
 {
